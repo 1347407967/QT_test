93cf13a299d38602b8777077a682f9e106daf6cd
JKchen <1347407967@qq.com>
2020-11-28 15:51:42 +0800

    界面

--- app/src/main/java/com/example/qt/MainActivity.java
+++ app/src/main/java/com/example/qt/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.qt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -34,10 +35,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         init();
 
+
+
     }
 
+
+
     private void init() {
-        fl_body = findViewById(R.id.fl_body);//界面
         /*
         * 文字*/
         tv_main = findViewById(R.id.tv_main);
@@ -55,8 +59,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         rl_my = findViewById(R.id.rl_my);
         rl_sq = findViewById(R.id.rl_sq);
         rl_xx = findViewById(R.id.rl_xx);
-
+        initBodyLayout();
+        setListener();
     }
+
+
+
+    private void initBodyLayout() {
+        fl_body = findViewById(R.id.fl_body);//界面
+    }
+
+
 
     /*
     * 点击事件*/
@@ -64,16 +77,73 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.rl_main:
+                cleaBottomImageState();
+                selectDisplayview(1);
                 break;
             case R.id.rl_sq:
+                cleaBottomImageState();
                 break;
             case R.id.rl_xx:
+                cleaBottomImageState();
                 break;
             case R.id.rl_my:
+                cleaBottomImageState();
                 break;
                 default:
                     break;
 
+        }
+    }
+    /*
+    * 设置底部控件点击的监听事件*/
+    private void setListener() {
+    for(int i=0;i<fl_body.getChildCount();i++){
+        fl_body.getChildAt(i).setOnClickListener(this);
+    }
+    }
+
+    /*
+    * 显示相应的界面*/
+    private void selectDisplayview(int i) {
+        removeAllView();
+        createView(i);
+    }
+    /*
+    * 选择视图*/
+    private void createView(int viewIndex) {
+        switch (viewIndex){
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+
+    }
+
+
+    /*
+    * 移除不需要的视图*/
+    private void removeAllView() {
+        for(int i=0;i<fl_body.getChildCount();i++){
+            fl_body.getChildAt(i).setVisibility(View.GONE);
+        }
+    }
+
+    private void cleaBottomImageState() {
+        tv_main.setTextColor(Color.parseColor("#666666"));
+        tv_my.setTextColor(Color.parseColor("#666666"));
+        tv_sq.setTextColor(Color.parseColor("#666666"));
+        tv_xx.setTextColor(Color.parseColor("#666666"));
+        iv_main.setImageResource(R.drawable.sy_icon_1);
+        iv_my.setImageResource(R.drawable.my_icon_1);
+        iv_sq.setImageResource(R.drawable.sq_icon_1);
+        iv_xx.setImageResource(R.drawable.xx_icon_1);
+        for(int i=0;i<fl_body.getChildCount();i++){
+            fl_body.getChildAt(i).setSelected(false);
         }
     }
 }
